78452c0ddd96baee490a5de90ba2abeba5600a1f
Emerson <eemr3@yahoo.com.br>
2021-02-23 14:34:30 -0300

    build: componente Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/eemr3.png" alt="Emerson Moreira" />
+      <div>
+        <strong>Emerson Moreira</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
