5083777df6bedeb4b809c8dfae2dd23430eaff73
Emerson <eemr3@yahoo.com.br>
2021-02-25 15:07:10 -0300

    refector: ajustes no contador

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -23,7 +23,7 @@ interface CountdownProviderProps {
 export const CountdownContext = createContext({} as CountdownContexData);
 
 export function CountdownProvider({ children }: CountdownProviderProps) {
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -43,7 +43,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     clearTimeout(countdownTime);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
   }
 
   useEffect(() => {
